3ca9d4cd9feeebd1169c092a9001db5e4896c3e4
DuongQuan <duongvuvietquan2912@gmail.com>
2022-09-12 02:13:01 +0700

    BE initial commit

---
+++ BE/online-learning-system/src/test/java/swp490/g23/onlinelearningsystem/OnlineLearningSystemApplicationTests.java
@@ -0,0 +1,13 @@
+package swp490.g23.onlinelearningsystem;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class OnlineLearningSystemApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
