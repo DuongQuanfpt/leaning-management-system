f7b0dcb2fd9b2b1c7467ae090fd89245959aa0bc
DuongQuan <duongvuvietquan2912@gmail.com>
2022-09-19 23:31:29 +0700

    push to heroku

--- src/main/java/swp490/g23/onlinelearningsystem/OnlineLearningSystemApplication.java
+++ src/main/java/swp490/g23/onlinelearningsystem/OnlineLearningSystemApplication.java
@@ -9,15 +9,15 @@ import org.springframework.web.servlet.config.annotation.WebMvcConfigurer;
 @SpringBootApplication
 public class OnlineLearningSystemApplication {
 
-	@Bean
-	public WebMvcConfigurer mvcConfigurer(){//Enable CORS
-        return new WebMvcConfigurer() {
-            @Override
-            public void addCorsMappings(CorsRegistry registry) {
-              registry.addMapping("/*").allowedOriginPatterns("http://localhost:3000");
-            }
-        };
-    }
+	// @Bean
+	// public WebMvcConfigurer mvcConfigurer(){//Enable CORS
+    //     return new WebMvcConfigurer() {
+    //         @Override
+    //         public void addCorsMappings(CorsRegistry registry) {
+    //           registry.addMapping("/*").allowedOriginPatterns("http://localhost:3000");
+    //         }
+    //     };
+    // }
 
 	public static void main(String[] args) {
 		SpringApplication.run(OnlineLearningSystemApplication.class, args);
--- src/main/java/swp490/g23/onlinelearningsystem/entities/user/controller/UserController.java
+++ src/main/java/swp490/g23/onlinelearningsystem/entities/user/controller/UserController.java
@@ -6,6 +6,7 @@ import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.ResponseEntity;
 import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder;
 import org.springframework.security.crypto.password.PasswordEncoder;
+import org.springframework.web.bind.annotation.CrossOrigin;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.PutMapping;
@@ -21,6 +22,7 @@ import swp490.g23.onlinelearningsystem.entities.user.service.impl.UserService;
 
 @RestController
 @RequestMapping("/user")
+@CrossOrigin(origins = "http://localhost:3000")
 public class UserController {
 	@Autowired
 	private UserService userService;
