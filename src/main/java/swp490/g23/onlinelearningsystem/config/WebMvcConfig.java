791d9f91e5531f00026b67ef0dd2756c49d6bdfa
DuongQuan <duongvuvietquan2912@gmail.com>
2022-09-19 23:53:56 +0700

    push to heroku

--- src/main/java/swp490/g23/onlinelearningsystem/config/WebMvcConfig.java
+++ src/main/java/swp490/g23/onlinelearningsystem/config/WebMvcConfig.java
@@ -10,7 +10,7 @@ public class WebMvcConfig implements WebMvcConfigurer {
     @Override
     public void addCorsMappings(CorsRegistry registry) {
         registry.addMapping("/**")
-        		.allowedOriginPatterns("http://localhost:3000")
+        		.allowedOriginPatterns("*")
         		.allowedHeaders("*")
         		.allowedMethods("*")
                 ;
