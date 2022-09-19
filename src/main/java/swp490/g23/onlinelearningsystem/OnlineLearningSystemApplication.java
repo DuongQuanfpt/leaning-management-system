6a8703acb8f791cc720d36dcb985b47d0c9e4b3d
DuongQuan <duongvuvietquan2912@gmail.com>
2022-09-19 23:50:38 +0700

    push to heroku

--- src/main/java/swp490/g23/onlinelearningsystem/OnlineLearningSystemApplication.java
+++ src/main/java/swp490/g23/onlinelearningsystem/OnlineLearningSystemApplication.java
@@ -8,17 +8,6 @@ import org.springframework.web.servlet.config.annotation.WebMvcConfigurer;
 
 @SpringBootApplication
 public class OnlineLearningSystemApplication {
-
-	@Bean
-	public WebMvcConfigurer mvcConfigurer(){//Enable CORS
-        return new WebMvcConfigurer() {
-            @Override
-            public void addCorsMappings(CorsRegistry registry) {
-              registry.addMapping("/*").allowedOriginPatterns("http://localhost:3000");
-            }
-        };
-    }
-
 	public static void main(String[] args) {
 		SpringApplication.run(OnlineLearningSystemApplication.class, args);
 
---
+++ src/main/java/swp490/g23/onlinelearningsystem/config/WebMvcConfig.java
@@ -0,0 +1,19 @@
+package swp490.g23.onlinelearningsystem.config;
+
+import org.springframework.context.annotation.Configuration;
+import org.springframework.web.servlet.config.annotation.CorsRegistry;
+import org.springframework.web.servlet.config.annotation.WebMvcConfigurer;
+
+@Configuration
+public class WebMvcConfig implements WebMvcConfigurer {
+
+    @Override
+    public void addCorsMappings(CorsRegistry registry) {
+        registry.addMapping("/**")
+        		.allowedOriginPatterns("http://localhost:3000")
+        		.allowedHeaders("*")
+        		.allowedMethods("*")
+                ;
+    }
+    
+}
