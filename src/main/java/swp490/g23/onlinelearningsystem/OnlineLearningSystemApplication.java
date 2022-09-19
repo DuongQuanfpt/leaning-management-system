5d0125e88c0afca8fcac79abc0b54cf55f8ee7e7
DuongQuan <duongvuvietquan2912@gmail.com>
2022-09-19 23:21:01 +0700

    push to heroku

--- src/main/java/swp490/g23/onlinelearningsystem/OnlineLearningSystemApplication.java
+++ src/main/java/swp490/g23/onlinelearningsystem/OnlineLearningSystemApplication.java
@@ -2,9 +2,22 @@ package swp490.g23.onlinelearningsystem;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.context.annotation.Bean;
+import org.springframework.web.servlet.config.annotation.CorsRegistry;
+import org.springframework.web.servlet.config.annotation.WebMvcConfigurer;
 
 @SpringBootApplication
 public class OnlineLearningSystemApplication {
+
+	@Bean
+	public WebMvcConfigurer mvcConfigurer(){//Enable CORS
+        return new WebMvcConfigurer() {
+            @Override
+            public void addCorsMappings(CorsRegistry registry) {
+              registry.addMapping("/*").allowedOriginPatterns("http://localhost:3000/");
+            }
+        };
+    }
 
 	public static void main(String[] args) {
 		SpringApplication.run(OnlineLearningSystemApplication.class, args);
--- src/main/java/swp490/g23/onlinelearningsystem/entities/auth/controller/AuthController.java
+++ src/main/java/swp490/g23/onlinelearningsystem/entities/auth/controller/AuthController.java
@@ -5,17 +5,18 @@ import javax.validation.Valid;
 import org.springframework.beans.factory.annotation.Autowired;
 
 import org.springframework.http.ResponseEntity;
-
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
+
 
 import swp490.g23.onlinelearningsystem.entities.auth.domain.AuthRequest;
 import swp490.g23.onlinelearningsystem.entities.auth.service.impl.AuthService;
 
 @RestController
 @RequestMapping("/auth")
+
 public class AuthController {
 
     @Autowired
