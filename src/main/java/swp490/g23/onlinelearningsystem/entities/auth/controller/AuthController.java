234e1d3a4afb0a15a6dc7ced6b83ba721172bbee
DuongQuan <duongvuvietquan2912@gmail.com>
2022-09-19 23:32:54 +0700

    push to heroku

--- src/main/java/swp490/g23/onlinelearningsystem/entities/auth/controller/AuthController.java
+++ src/main/java/swp490/g23/onlinelearningsystem/entities/auth/controller/AuthController.java
@@ -5,6 +5,7 @@ import javax.validation.Valid;
 import org.springframework.beans.factory.annotation.Autowired;
 
 import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.CrossOrigin;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
@@ -16,6 +17,7 @@ import swp490.g23.onlinelearningsystem.entities.auth.service.impl.AuthService;
 
 @RestController
 @RequestMapping("/auth")
+@CrossOrigin(origins = "http://localhost:3000")
 public class AuthController {
 
     @Autowired
