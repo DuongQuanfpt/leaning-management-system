45b72212bf7c172922b4370abcd01cbd90a816fe
chinhndse05562 <68887991+chinhndse05562@users.noreply.github.com>
2022-10-09 21:17:09 +0700

    update entity

--- BE/online-learning-system/src/main/java/swp490/g23/onlinelearningsystem/entities/class_user/domain/ClassUserKey.java
+++ BE/online-learning-system/src/main/java/swp490/g23/onlinelearningsystem/entities/class_user/domain/ClassUserKey.java
@@ -23,5 +23,36 @@ public class ClassUserKey implements Serializable{
     @Column(name = "user_id")
     private Long userId;
 
+    @Override
+    public int hashCode() {
+        final int prime = 31;
+        int result = 1;
+        result = prime * result + ((classId == null) ? 0 : classId.hashCode());
+        result = prime * result + ((userId == null) ? 0 : userId.hashCode());
+        return result;
+    }
 
+    @Override
+    public boolean equals(Object obj) {
+        if (this == obj)
+            return true;
+        if (obj == null)
+            return false;
+        if (getClass() != obj.getClass())
+            return false;
+        ClassUserKey other = (ClassUserKey) obj;
+        if (classId == null) {
+            if (other.classId != null)
+                return false;
+        } else if (!classId.equals(other.classId))
+            return false;
+        if (userId == null) {
+            if (other.userId != null)
+                return false;
+        } else if (!userId.equals(other.userId))
+            return false;
+        return true;
+    }
+
+    
 }
