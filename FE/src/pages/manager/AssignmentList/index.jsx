0d8d50ddf3b1f7179f9b20f9b38d34743d41e42a
Hoang <hoangnh2000.dev@gmail.com>
2022-10-23 02:24:45 +0700

    Update AssignmentList

--- FE/src/pages/manager/AssignmentList/index.jsx
+++ FE/src/pages/manager/AssignmentList/index.jsx
@@ -1,7 +1,300 @@
-import React from 'react'
+import React, { useState, useEffect } from 'react'
+import { Link, useNavigate } from 'react-router-dom'
+import { useSelector } from 'react-redux'
+
+import { Breadcrumb, Button, Modal, Pagination, Space, Table, Tag, Tooltip } from 'antd'
+import { CheckOutlined, CloseOutlined, ExclamationCircleOutlined, EyeOutlined } from '@ant-design/icons'
+
+import { CButton, CDropdown, CDropdownItem, CDropdownMenu, CDropdownToggle } from '@coreui/react'
+import CIcon from '@coreui/icons-react'
+import { cilPlus, cilSearch, cilSync } from '@coreui/icons'
+
+import assignmentApi from '~/api/assignmentApi'
+
+import AdminHeader from '~/components/AdminDashboard/AdminHeader'
+import AdminSidebar from '~/components/AdminDashboard/AdminSidebar'
+import AdminFooter from '~/components/AdminDashboard/AdminFooter'
 
 const AssignmentList = () => {
-  return <div>AssignmentList</div>
+  const ITEM_PER_PAGE = 10
+  const navigateTo = useNavigate()
+
+  const [listAssignment, setListAssignment] = useState([])
+  const [totalItem, setTotalItem] = useState(1)
+  const [currentPage, setCurrentPage] = useState(1)
+
+  const [search, setSearch] = useState('')
+  const [listFilter, setListFilter] = useState({
+    subjectFilter: [],
+    statusFilter: [],
+  })
+
+  const [filter, setFilter] = useState({
+    subject: 'Select Subject',
+    status: {
+      name: 'Select Status',
+      value: '',
+    },
+  })
+
+  useEffect(() => {
+    assignmentApi
+      .getFilter()
+      .then((response) => {
+        setListFilter((prev) => ({
+          ...prev,
+          subjectFilter: response.subjectFilter,
+          statusFilter: response.statusFilter,
+        }))
+      })
+      .catch((error) => {
+        console.log(error)
+      })
+  }, [])
+
+  useEffect(() => {
+    loadData(1, filter)
+    // eslint-disable-next-line react-hooks/exhaustive-deps
+  }, [filter])
+
+  const loadData = async (page, fiter, q = '') => {
+    const params = {
+      item: ITEM_PER_PAGE,
+      page: page,
+    }
+    if (q !== '') {
+      params.q = q.trim()
+    }
+    if (filter.subject !== 'Select Subject') {
+      params.filterSubject = filter.subject
+    }
+    if (filter.status.name !== 'Select Status') {
+      params.filterStatus = filter.status.value
+    }
+
+    await assignmentApi
+      .getPage(params)
+      .then((response) => {
+        setListAssignment(response.listResult)
+        setCurrentPage(page)
+        setTotalItem(response.totalItem)
+      })
+      .catch((error) => {
+        console.log(error)
+      })
+  }
+
+  const handleSearch = () => {
+    loadData(1, filter, search)
+  }
+
+  const handleFilterSubject = (subject) => {
+    setFilter((prev) => ({ ...prev, subject: subject }))
+  }
+
+  const handleFilterStatus = (status) => {
+    setFilter((prev) => ({ ...prev, status: { ...prev.status, name: status.name, value: status.value } }))
+  }
+
+  const handleReload = () => {
+    setSearch('')
+    setFilter({
+      subject: 'Select Subject',
+      status: {
+        name: 'Select Status',
+        value: '',
+      },
+    })
+  }
+
+  const handleAdd = () => {
+    navigateTo('/assignment-add')
+  }
+
+  const handleChangePage = (pageNumber) => {
+    setCurrentPage(pageNumber)
+    loadData(pageNumber, filter)
+  }
+
+  const handleActive = async (id) => {
+    await assignmentApi
+      .changeActive(id)
+      .then((response) => {
+        loadData(currentPage, filter)
+      })
+      .catch((error) => {
+        console.log(error)
+      })
+  }
+
+  const modalConfirm = (subject) => {
+    Modal.confirm({
+      title: `Are you want to ${subject.status === 'Active' ? 'deactivate' : 'reactivate'} "${subject.title}" - "${
+        subject.assBody
+      }" ?`,
+      icon: <ExclamationCircleOutlined />,
+      okText: 'OK',
+      cancelText: 'Cancel',
+      okType: 'danger',
+      onOk() {
+        handleActive(subject.assId)
+      },
+      onCancel() {},
+    })
+  }
+
+  const columns = [
+    {
+      title: 'Subject',
+      dataIndex: 'subjectName',
+      sorter: (a, b) => a.subjectName.length - b.subjectName.length,
+      width: '10%',
+    },
+    {
+      title: 'Title',
+      dataIndex: 'title',
+      sorter: (a, b) => a.title.length - b.title.length,
+      width: '15%',
+    },
+    {
+      title: 'Body',
+      dataIndex: 'assBody',
+      sorter: (a, b) => a.assBody.length - b.assBody.length,
+      width: '25%',
+    },
+    {
+      title: 'Status',
+      dataIndex: 'status',
+      width: '10%',
+      sorter: (a, b) => a.status?.length - b.status?.length,
+      render: (_, { status }) => (
+        <Tag color={status === 'Active' ? 'blue' : status === 'Inactive' ? 'red' : 'grey'} key={status}>
+          {status}
+        </Tag>
+      ),
+    },
+    {
+      title: 'Weight',
+      dataIndex: 'eval_weight',
+      sorter: (a, b) => a.eval_weight.length - b.eval_weight.length,
+      width: '10%',
+    },
+    {
+      title: 'Is Teamwork',
+      dataIndex: 'isTeamWork',
+      sorter: (a, b) => a.isTeamWork.length - b.isTeamWork.length,
+      width: '10%',
+      render: (_, { isTeamWork }) => (isTeamWork === 1 ? 'Yes' : 'No'),
+    },
+    {
+      title: 'Is Ongoing',
+      dataIndex: 'isOnGoing',
+      sorter: (a, b) => a.isOnGoing.length - b.isOnGoing.length,
+      width: '10%',
+      render: (_, { isOnGoing }) => (isOnGoing === 1 ? 'Yes' : 'No'),
+    },
+    {
+      title: 'Actions',
+      dataIndex: 'actions',
+      width: '10%',
+      render: (_, subject) => (
+        <Space size="middle" align="baseline">
+          <Tooltip title={subject.status === 'Active' ? 'Deactivate' : 'Reactivate'} placement="top">
+            <Button
+              type={subject.status === 'Active' ? 'danger' : 'primary'}
+              shape="circle"
+              icon={subject.status === 'Active' ? <CloseOutlined /> : <CheckOutlined />}
+              onClick={() => {
+                modalConfirm(subject)
+              }}
+            ></Button>
+          </Tooltip>
+          <Tooltip title="View" placement="top">
+            <Button
+              shape="circle"
+              icon={<EyeOutlined />}
+              onClick={() => {
+                navigateTo(`/assignment-detail/${subject?.assId}`)
+              }}
+            ></Button>
+          </Tooltip>
+        </Space>
+      ),
+    },
+  ]
+
+  return (
+    <div>
+      <AdminSidebar />
+      <div className="wrapper d-flex flex-column min-vh-100 bg-light">
+        <AdminHeader />
+        <div className="col-lg-12 m-b30">
+          <div className="row">
+            <div className="col-lg-12 m-b30">
+              <div className="row">
+                <div className="col-2 d-flex align-items-center">
+                  <Breadcrumb>
+                    <Breadcrumb.Item>
+                      <Link to="/dashboard">Dashboard</Link>
+                    </Breadcrumb.Item>
+                    <Breadcrumb.Item>Class Setting List</Breadcrumb.Item>
+                  </Breadcrumb>
+                </div>
+                <div className="col-4 d-flex w-80">
+                  <input
+                    type="search"
+                    id="form1"
+                    className="form-control"
+                    placeholder="Searching by Class title..."
+                    value={search}
+                    onChange={(e) => setSearch(e.target.value)}
+                  />
+                  <CButton color="primary" type="submit" className="text-light ml-10" onClick={handleSearch}>
+                    <CIcon icon={cilSearch} />
+                  </CButton>
+                </div>
+                <div className="col-6 d-flex justify-content-end">
+                  <CDropdown className="ml-4">
+                    <CDropdownToggle color="secondary">{filter.subject}</CDropdownToggle>
+                    <CDropdownMenu>
+                      {listFilter?.subjectFilter?.map((subject) => (
+                        <CDropdownItem onClick={() => handleFilterSubject(subject)}>{subject}</CDropdownItem>
+                      ))}
+                    </CDropdownMenu>
+                  </CDropdown>
+                  <CDropdown className="ml-4">
+                    <CDropdownToggle color="secondary">{filter.status.name}</CDropdownToggle>
+                    <CDropdownMenu>
+                      {listFilter?.statusFilter?.map((status) => (
+                        <CDropdownItem onClick={() => handleFilterStatus(status)}>{status.name}</CDropdownItem>
+                      ))}
+                    </CDropdownMenu>
+                  </CDropdown>
+                  <Tooltip title="Reload" placement="top">
+                    <CButton color="success" type="submit" className="text-light ml-4" onClick={handleReload}>
+                      <CIcon icon={cilSync} />
+                    </CButton>
+                  </Tooltip>
+                  <Tooltip title="Add New Assignment" placement="right">
+                    <CButton color="danger" type="submit" className="text-light ml-4" onClick={handleAdd}>
+                      <CIcon icon={cilPlus} />
+                    </CButton>
+                  </Tooltip>
+                </div>
+              </div>
+            </div>
+            <div className="col-lg-12">
+              <Table bordered dataSource={listAssignment} columns={columns} pagination={false} />
+            </div>
+            <div className="col-lg-12 d-flex justify-content-end">
+              <Pagination current={currentPage} total={totalItem} onChange={handleChangePage} />;
+            </div>
+          </div>
+        </div>
+      </div>
+      <AdminFooter />
+    </div>
+  )
 }
 
 export default AssignmentList
