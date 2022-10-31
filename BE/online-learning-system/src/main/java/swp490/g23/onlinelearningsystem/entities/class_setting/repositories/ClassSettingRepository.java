238e8324e7a87635bb536c9b96c6c49cb2fed421
chinhndse05562 <68887991+chinhndse05562@users.noreply.github.com>
2022-11-01 04:00:34 +0700

    update BE add schedule

--- BE/online-learning-system/src/main/java/swp490/g23/onlinelearningsystem/entities/class_setting/repositories/ClassSettingRepository.java
+++ BE/online-learning-system/src/main/java/swp490/g23/onlinelearningsystem/entities/class_setting/repositories/ClassSettingRepository.java
@@ -6,10 +6,9 @@ import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.data.jpa.repository.Query;
 
 import swp490.g23.onlinelearningsystem.entities.class_setting.domain.ClassSetting;
-import swp490.g23.onlinelearningsystem.entities.classes.domain.Classes;
 
 public interface ClassSettingRepository extends JpaRepository<ClassSetting, Long> {
 
-    @Query(value = "SELECT c FROM ClassSetting c WHERE c.type.settingValue = 'TYPE_CLASS_MODULE' AND c.classes = :clazz")
-    List<ClassSetting> findByClassAndSlot(Classes clazz);
+    @Query(value = "SELECT c FROM ClassSetting c WHERE c.type.settingValue = 'TYPE_CLASS_MODULE' AND c.classes.code = :clazz")
+    List<ClassSetting> findByClassAndSlot(String clazz);
 }
--- BE/online-learning-system/src/main/java/swp490/g23/onlinelearningsystem/entities/schedule/domain/response/ModuleTypeResponseDTO.java
+++ BE/online-learning-system/src/main/java/swp490/g23/onlinelearningsystem/entities/schedule/domain/response/ModuleTypeResponseDTO.java
@@ -12,4 +12,5 @@ import lombok.Setter;
 public class ModuleTypeResponseDTO {
     private String slot;
     private String topic;
+    private String classCode;
 }
