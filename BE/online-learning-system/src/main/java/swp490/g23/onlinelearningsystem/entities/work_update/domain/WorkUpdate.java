20cf368cfc7133e679578faa9f2c305fb51ddd8f
DuongQuan <duongvuvietquan2912@gmail.com>
2022-11-20 02:16:51 +0700

    Code BE : merge code

--- BE/online-learning-system/src/main/java/swp490/g23/onlinelearningsystem/entities/work_update/domain/WorkUpdate.java
+++ BE/online-learning-system/src/main/java/swp490/g23/onlinelearningsystem/entities/work_update/domain/WorkUpdate.java
@@ -1,5 +1,7 @@
 package swp490.g23.onlinelearningsystem.entities.work_update.domain;
 
+import java.time.LocalDateTime;
+
 import javax.persistence.Column;
 import javax.persistence.Entity;
 import javax.persistence.GeneratedValue;
@@ -11,6 +13,7 @@ import lombok.AllArgsConstructor;
 import lombok.Getter;
 import lombok.NoArgsConstructor;
 import lombok.Setter;
+import swp490.g23.onlinelearningsystem.entities.BaseEntity;
 
 @Entity
 @Table(name = "work_update_tbl")
@@ -18,10 +21,15 @@ import lombok.Setter;
 @Setter
 @NoArgsConstructor
 @AllArgsConstructor
-public class WorkUpdate {
+public class WorkUpdate extends BaseEntity {
     @Id
     @GeneratedValue(strategy = GenerationType.IDENTITY)
     private Long workUpdateId;
+
+    @ManyToOne
+    @JoinColumn(name = "milestone_id")
+    private Milestone milestone;
+
 
     @Column
     private String title;
@@ -30,5 +38,5 @@ public class WorkUpdate {
     private String description;
 
     @Column
-    private String submitFileUrl;
+    private LocalDateTime updateDate;
 }
